951bfd4cf6ea9aed41418c9634c479336fef791d
Юрий Корс <mastrayane@kors.ubuntu>
2025-04-01 22:58:29 +0300

    Добавдены комментарии

--- androidwindow.h
+++ androidwindow.h
@@ -11,7 +11,6 @@
 #include <QPointer>
 #include <functional>
 #include <QPauseAnimation>
-
 #include <QtConcurrent/QtConcurrent>
 #include <QMutex>
 #include <QFutureWatcher>
@@ -22,12 +21,12 @@ class AndroidWindow;
 
 // Структура для параметров анимации каждого кадра
 struct AnimationFrame {
-    QString imagePath;
-    float vignetteIntensity;
-    float vignetteRadius;
-    int duration;
-    bool hideWidgets;
-    QString debugName; // Добавляем поле для отладочной информации
+    QString imagePath;          // Путь к изображению кадра
+    float vignetteIntensity;    // Интенсивность виньетирования
+    float vignetteRadius;       // Радиус виньетирования
+    int duration;               // Длительность анимации (мс)
+    bool hideWidgets;           // Флаг скрытия виджетов во время анимации
+    QString debugName;          // Отладочное имя кадра
 };
 
 class AndroidWindow : public QMainWindow
@@ -39,51 +38,53 @@ public:
     ~AndroidWindow();
 
     void SetInterface(); // Инициализация интерфейса
-    void PreloadAnimationFrames(); //Предзагрузка изоюоажений в кэш
+    void PreloadAnimationFrames(); // Предзагрузка изображений для анимации в кэш
 
 private:
-    // Методы работы с изображениями
-    void SetPixmap(const QString path);
-    QPixmap applyVignetteEffect(const QPixmap& original, float intensity = 0.7f, float radius = 0.5f);
-    void SetPixmapWithVignette(const QString& path, float intensity = 0.7f, float radius = 0.5f);
-    void SetPixmap(QWidget* widget, const QString& path, double multiplicity_size = 1.0);
-    void FitImage();
-    void SetFolder(const QString& d);
 
-    // Методы анимации
-    void crossFadeToImage(const QString& newImagePath, float intensity, float radius, int duration);
-    void addComplexTransition(QSequentialAnimationGroup* group, const AnimationFrame& frame);
-    QLabel* createOverlayLabel();
+     //--- МЕТОДЫ РАБОТЫ С ИЗОБРАЖЕНИЯМИ ---
+    void SetPixmap(const QString path); // Установка фонового изображения
+    QPixmap applyVignetteEffect(const QPixmap& original, float intensity = 0.7f, float radius = 0.5f); // Применение виньетки
+    void SetPixmapWithVignette(const QString& path, float intensity = 0.7f, float radius = 0.5f); // Установка изображения с виньеткой
+    void SetPixmap(QWidget* widget, const QString& path, double multiplicity_size = 1.0); // Установка изображения для виджета
+    void FitImage();  // Подгонка изображения под размер окна
+    void SetFolder(const QString& d); // Установка фоновой папки
 
+     //--- МЕТОДЫ АНИМАЦИИ ---
+    void crossFadeToImage(const QString& newImagePath, float intensity, float radius, int duration); // Плавный переход между изображениями
+    void addComplexTransition(QSequentialAnimationGroup* group, const AnimationFrame& frame); // Добавление сложного перехода
+    QLabel* createOverlayLabel(); // Создание наложения для анимации
+    void fadeWidgets(bool fadeIn, int duration, std::function<void()> callback = nullptr); // Плавное появление/исчезновение виджетов
 
-    // Вспомогательные методы
-    void UpdateQLabelSize();
-    void MakeCalculations();
-    QList<QWidget*> getAllWidgets();
-    void setWidgetsVisible(bool visible);
+    //--- ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ ---
+    void MakeCalculations(); // Вычисление данных на основе введенных значений
+    QList<QWidget*> getAllWidgets(); // Получение списка всех виджетов окна
+    void setWidgetsVisible(bool visible); // Управление видимостью виджетов
 
-    // Обработчики событий
-    void resizeEvent(QResizeEvent *event) override;
-    void showEvent(QShowEvent *event) override;
+     //--- ОБРАБОТЧИКИ СОБЫТИЙ ---
+    void resizeEvent(QResizeEvent *event) override; // Изменение размера окна
+    void showEvent(QShowEvent *event) override; // Событие отображения окна
 
 private:
+
     Ui::AndroidWindow *ui;       // Указатель на UI
-    QPixmap active_pixmap;       // Текущее фоновое изображение
+    QPixmap active_pixmap;       // Текущее активное фоновое изображение
     QLabel lbl_new_{this};       // Метка для отображения фона
     QPointer<QSequentialAnimationGroup> currentAnimation; // Указатель на текущую анимацию
-    bool widgetsHidden = false; // отслеживания состояния виджетов
-    bool widgetsForceVisible = false; // Новый флаг принудительной видимости
+    bool widgetsHidden = false; // Флаг отслеживания состояния видимости виджетов
+    bool widgetsForceVisible = false; // Флаг принудительной видимости виджетов
 
-    // Кэш предзагрузки
-    QVector<AnimationFrame> m_animationFrames; // Храним параметры анимации
-    QMap<QString, QPixmap> m_animationCache; // Альтернативный кэш
+    //--- КЭШ ПРЕДЗАГРУЗКИ ---
+    QVector<AnimationFrame> m_animationFrames; // Список кадров анимации
+    QMap<QString, QPixmap> m_animationCache; // Кэш загруженных изображений
 
-    // Многопточность
-    QFutureWatcher<void> m_preloadWatcher;
-    QMutex m_cacheMutex;
-    bool m_essentialLoaded = false;
-    void asyncPreloadFrame(const AnimationFrame& frame);
-    void finalizePreload();
+   //--- МНОГОПОТОЧНОСТЬ ---
+    QFutureWatcher<void> m_preloadWatcher; // Наблюдатель за фоновой загрузкой
+    QMutex m_cacheMutex;  // Мьютекс для синхронизации кэша
+    bool m_essentialLoaded = false; // Флаг завершения начальной загрузки
+
+    void asyncPreloadFrame(const AnimationFrame& frame); // Асинхронная загрузка кадра
+    void finalizePreload(); // Завершение предзагрузки
 
 
 
@@ -91,8 +92,8 @@ private slots:
     void on_pushButton_clicked();    // Обработчик нажатия кнопки
     void on_pushButton_released();   // Обработчик отпускания кнопки
     void on_pushButton_pressed();    // Обработчик нажатия кнопки
-    void fadeWidgets(bool fadeIn, int duration, std::function<void()> callback = nullptr);
-     // Удаляем параметр по умолчанию из объявления
+
+
 };
 
 #endif // ANDROIDWINDOW_H
